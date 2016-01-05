f2b1d2b8beaa50eff0dfe316a36281ea178b2475
Weiran Zhang <weiran@face.eu.com>
2016-01-05 14:59:37 +0000

    Refactored into separate class files

--- LinqExpressionProjection/ProjectionSupportingQuery.cs
+++ LinqExpressionProjection/ProjectionSupportingQuery.cs
@@ -4,16 +4,14 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Collections;
 using System.Data.Entity.Infrastructure;
-using System.Threading.Tasks;
-using System.Threading;
 
 namespace LinqExpressionProjection
 {
-	/// <summary>
-	/// An IQueryable wrapper that allows us to visit the query's expression tree just before LINQ to SQL gets to it.
-	/// This is based on the excellent work of Tomas Petricek: http://tomasp.net/blog/linq-expand.aspx
-	/// </summary>
-	public class ProjectionSupportingQuery<T> : IQueryable<T>, IOrderedQueryable<T>, IOrderedQueryable, IDbAsyncEnumerable<T>
+    /// <summary>
+    /// An IQueryable wrapper that allows us to visit the query's expression tree just before LINQ to SQL gets to it.
+    /// This is based on the excellent work of Tomas Petricek: http://tomasp.net/blog/linq-expand.aspx
+    /// </summary>
+    public class ProjectionSupportingQuery<T> : IQueryable<T>, IOrderedQueryable<T>, IOrderedQueryable, IDbAsyncEnumerable<T>
     {
         ProjectionSupportingQueryProvider<T> _provider;
 		IQueryable<T> _inner;
@@ -36,76 +34,5 @@ namespace LinqExpressionProjection
         public IDbAsyncEnumerator GetAsyncEnumerator() { return GetAsyncEnumerator(); }
     }
 
-    class ProjectionSupportingQueryProvider<T> : IQueryProvider, IDbAsyncQueryProvider
-	{
-        private readonly ProjectionSupportingQuery<T> _query;
 
-        internal ProjectionSupportingQueryProvider(ProjectionSupportingQuery<T> query)
-		{
-			_query = query;
-		}
-
-		// The following four methods first call ExpressionExpander to visit the expression tree, then call
-		// upon the inner query to do the remaining work.
-
-		IQueryable<TElement> IQueryProvider.CreateQuery<TElement> (Expression expression)
-        {
-            return new ProjectionSupportingQuery<TElement>(_query.InnerQuery.Provider.CreateQuery<TElement>(expression.ExpandExpressionsForProjection()));
-		}
-
-		IQueryable IQueryProvider.CreateQuery (Expression expression)
-        {
-            return _query.InnerQuery.Provider.CreateQuery (expression.ExpandExpressionsForProjection());
-        }
-
-        object IQueryProvider.Execute(Expression expression)
-        {
-            return _query.InnerQuery.Provider.Execute(expression.ExpandExpressionsForProjection());
-        }
-
-        TResult IQueryProvider.Execute<TResult> (Expression expression)
-		{
-            return _query.InnerQuery.Provider.Execute<TResult>(expression.ExpandExpressionsForProjection());
-		}
-
-        Task<object> IDbAsyncQueryProvider.ExecuteAsync(Expression expression, CancellationToken cancellationToken)
-        {
-            return Task.FromResult(_query.InnerQuery.Provider.Execute(expression.ExpandExpressionsForProjection()));
-        }
-
-        Task<TResult> IDbAsyncQueryProvider.ExecuteAsync<TResult>(Expression expression, CancellationToken cancellationToken)
-        {
-            return Task.FromResult(_query.InnerQuery.Provider.Execute<TResult>(expression.ExpandExpressionsForProjection()));
-        }
-    }
-
-    class AsyncEnumerator<T> : IDbAsyncEnumerator<T>
-    {
-        private readonly IEnumerator<T> _inner;
-
-        public AsyncEnumerator(IEnumerator<T> inner)
-        {
-            _inner = inner;
-        }
-
-        public void Dispose()
-        {
-            _inner.Dispose();
-        }
-
-        public Task<bool> MoveNextAsync(CancellationToken cancellationToken)
-        {
-            return Task.FromResult(_inner.MoveNext());
-        }
-
-        public T Current
-        {
-            get { return _inner.Current; }
-        }
-
-        object IDbAsyncEnumerator.Current
-        {
-            get { return Current; }
-        }
-    }
 }
---
+++ LinqExpressionProjection/ProjectionSupportingQueryProvider.cs
@@ -0,0 +1,51 @@
+﻿using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LinqExpressionProjection
+{
+    internal class ProjectionSupportingQueryProvider<T> : IQueryProvider, IDbAsyncQueryProvider
+    {
+        private readonly ProjectionSupportingQuery<T> _query;
+
+        internal ProjectionSupportingQueryProvider(ProjectionSupportingQuery<T> query)
+        {
+            _query = query;
+        }
+
+        // The following four methods first call ExpressionExpander to visit the expression tree, then call
+        // upon the inner query to do the remaining work.
+
+        IQueryable<TElement> IQueryProvider.CreateQuery<TElement>(Expression expression)
+        {
+            return new ProjectionSupportingQuery<TElement>(_query.InnerQuery.Provider.CreateQuery<TElement>(expression.ExpandExpressionsForProjection()));
+        }
+
+        IQueryable IQueryProvider.CreateQuery(Expression expression)
+        {
+            return _query.InnerQuery.Provider.CreateQuery(expression.ExpandExpressionsForProjection());
+        }
+
+        object IQueryProvider.Execute(Expression expression)
+        {
+            return _query.InnerQuery.Provider.Execute(expression.ExpandExpressionsForProjection());
+        }
+
+        TResult IQueryProvider.Execute<TResult>(Expression expression)
+        {
+            return _query.InnerQuery.Provider.Execute<TResult>(expression.ExpandExpressionsForProjection());
+        }
+
+        Task<object> IDbAsyncQueryProvider.ExecuteAsync(Expression expression, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(_query.InnerQuery.Provider.Execute(expression.ExpandExpressionsForProjection()));
+        }
+
+        Task<TResult> IDbAsyncQueryProvider.ExecuteAsync<TResult>(Expression expression, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(_query.InnerQuery.Provider.Execute<TResult>(expression.ExpandExpressionsForProjection()));
+        }
+    }
+}
